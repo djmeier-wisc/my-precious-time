cbdb93896b4f6b42ec32c6cbc1cc42f24ca39cd2
djmeier-wisc <71796515+djmeier-wisc@users.noreply.github.com>
2023-10-27 19:46:52 -0500

    more config changes

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/frontend
-sudo npm run start > /home/ec2-user/logs/logs.txt 2> /home/ec2-user/logs/logs.txt < /home/ec2-user/logs/logs.txt &
+sudo npm i
+sudo npm run start > /home/ec2-user/logs/logs-fe.txt 2> /home/ec2-user/logs/logs-fe.txt < /home/ec2-user/logs/logs-fe.txt &
