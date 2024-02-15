39425cc9a4fe4250c9fd014bcb0ef7f485ebe55d
smmoon2 <smmoon2@wisc.edu>
2024-02-15 07:16:47 -0600

    Updates for use color

--- src/app/charting/colorCheckbox.js
+++ src/app/charting/colorCheckbox.js
@@ -3,28 +3,28 @@ import { Checkbox, FormControlLabel } from "@mui/material";
 import { useState } from "react";
 
 export default function ColorCheckbox() {
-  const [checked, setChecked] = useState(true);
-  const handleCheck = (
-    /** @type {{ target: { checked: boolean; }; }} */ box
-  ) => {
-    if (box.target.checked == true) {
-      // return without colors
-      console.log("checked!");
+  const [isChecked, setIsChecked] = useState(true);
+  const handleCheck = () => {
+    setIsChecked(!isChecked);
+    if (!isChecked) {
+      // return with colors - not sure why "not" actually *is* checked, but, whatever?
     } else {
       console.log("not!");
     }
   };
   return (
-    <input
-      name="checked"
-      type="checkbox"
-      checked={checked}
-      onChange={handleCheck}
-    />
+    <div id="checkbox-div">
+      <label htmlFor="checkbox" id="checkbox-label">
+        Use GTFS Colors
+      </label>
+      <br />
+      <input
+        name="checkbox"
+        id="checkbox"
+        type="checkbox"
+        checked={isChecked}
+        onChange={handleCheck}
+      />
+    </div>
   );
 }
-//let handleChange = (event) => {
-//   const {
-//     target: { value },
-// } = event;
-// setCurrBusList(typeof value === 'string' ? value.split(',') : value);
